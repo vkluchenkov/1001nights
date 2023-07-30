f0aa6a4ee628e292ffc8e2f5569f1b4e8129434f
Vladimir Kluchenkov <vkluchenkov@gmail.com>
2023-07-30 13:33:36 +0200

    feat: change styling for workshops + text

--- pages/workshops/index.tsx
+++ pages/workshops/index.tsx
@@ -24,32 +24,34 @@ const Workshops: NextPage = () => {
       <div className='content__container workshops__container'>
         <h1 className='main__header'>Termine und Workshops&nbsp;2023</h1>
         <section className='workshops__section'>
-          <h2 className='main__subheader'>Freitag 06.10.2023</h2>
+          <h2 className='main__subheader workshops__white'>Freitag 06.10.2023</h2>
           <ul className='workshops__items'>
             <li className='workshops__item'>
               <p className='workshops__item-time'>09:00-11:00</p>
               <p className='workshops__item-name'>Rayana</p>
-              <p className='workshops__item-topic'>Saidi</p>
+              <p className='workshops__item-topic'>Saidi Choreografie</p>
             </li>
             <li className='workshops__item'>
               <p className='workshops__item-time'>11:15-13:15</p>
               <p className='workshops__item-name'>Yela</p>
-              <p className='workshops__item-topic'>Schleierfächer</p>
+              <p className='workshops__item-topic'>Schleierfächer Choreografie</p>
             </li>
             <li className='workshops__item'>
               <p className='workshops__item-time'>13:30-15:30</p>
               <p className='workshops__item-name'>Leandro Ferreyra</p>
-              <p className='workshops__item-topic'>Tango oriental</p>
+              <p className='workshops__item-topic'>Tango oriental Choreografie</p>
             </li>
             <li className='workshops__item'>
               <p className='workshops__item-time'>19:30</p>
               <p className='workshops__item-name'>1001Nacht Opening Gala Show</p>
-              <p className='workshops__item-topic'>Ort: Am Alten Gymnasium 1, Neuruppin</p>
+              <p className='workshops__item-topic workshops__white'>
+                Ort: Am Alten Gymnasium 1, Neuruppin
+              </p>
             </li>
           </ul>
         </section>
         <section className='workshops__section'>
-          <h2 className='main__subheader'>Samstag 07.10.2023</h2>
+          <h2 className='main__subheader workshops__white'>Samstag 07.10.2023</h2>
           <ul className='workshops__items'>
             <li className='workshops__item'>
               <p className='workshops__item-time'>Ab 09:00-18:00</p>
@@ -58,36 +60,41 @@ const Workshops: NextPage = () => {
             <li className='workshops__item'>
               <p className='workshops__item-time'>19:30</p>
               <p className='workshops__item-name'>1001Nacht Stars Gala Show</p>
-              <p className='workshops__item-topic'>Ort: Virchowstr. 41, Neuruppin</p>
+              <p className='workshops__item-topic workshops__white'>
+                Ort: Virchowstr. 41, Neuruppin
+              </p>
             </li>
           </ul>
         </section>
         <section className='workshops__section'>
-          <h2 className='main__subheader'>Sonntag 08.10.2023</h2>
+          <h2 className='main__subheader workshops__white'>Sonntag 08.10.2023</h2>
           <ul className='workshops__items'>
             <li className='workshops__item'>
               <p className='workshops__item-time'>09:00-11:00</p>
               <p className='workshops__item-name'>1. Rayana</p>
-              <p className='workshops__item-topic'>Tabla Solo</p>
+              <p className='workshops__item-topic'>Tabla Solo Choreografie</p>
               <p className='workshops__item-name'>2. Marina</p>
-              <p className='workshops__item-topic'>Baladi</p>
+              <p className='workshops__item-topic'>Baladi Choreografie</p>
             </li>
             <li className='workshops__item'>
               <p className='workshops__item-time'>11:15-13:15</p>
               <p className='workshops__item-name'>Shalymar</p>
-              <p className='workshops__item-topic'>Schleier Traum</p>
+              <p className='workshops__item-topic'>Schleier Traum Choreografie</p>
             </li>
             <li className='workshops__item'>
               <p className='workshops__item-time'>13:30-15:30</p>
               <p className='workshops__item-name'>Leandro Ferreyra</p>
-              <p className='workshops__item-topic'>Mejance</p>
+              <p className='workshops__item-topic'>Mejance Choreografie</p>
             </li>
             <li className='workshops__item'>
               <p className='workshops__item-time'>15:45-17:45</p>
               <p className='workshops__item-name'>1. Amira</p>
-              <p className='workshops__item-topic'>Pop</p>
+              <p className='workshops__item-topic'>Pop Choreografie</p>
               <p className='workshops__item-name'>2. Aminahbanu </p>
-              <p className='workshops__item-topic'>Grundschritte</p>
+              <p className='workshops__item-topic'>Grundschritte + kl. TrommelSolo Choreografie</p>
+              <p className='workshops__item-topic workshops__white'>
+                Ort: Am Alten Gymnasium 1, Neuruppin
+              </p>
             </li>
           </ul>
         </section>
