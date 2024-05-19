653b79687383c4f653799b7caece3cc5638116ec
Vladimir Kluchenkov <vkluchenkov@gmail.com>
2024-05-19 21:15:46 +0200

    feat: Update price display

--- pages/workshops/index.tsx
+++ pages/workshops/index.tsx
@@ -22,65 +22,57 @@ const Workshops: NextPage = () => {
         <meta name='description' content='1001 Nacht festival' />
       </Head>
       <div className='content__container workshops__container'>
-        <h1 className='main__header'>Termine und Workshops&nbsp;2023</h1>
+        <h1 className='main__header'>Termine und Workshops&nbsp;2024</h1>
         <section className='workshops__section'>
-          <h2 className='main__subheader workshops__white'>Freitag 06.10.2023</h2>
+          <h2 className='main__subheader workshops__white'>Freitag 11.10.2024</h2>
           <ul className='workshops__items'>
             <li className='workshops__item'>
-              <p className='workshops__item-time'>09:00-11:00</p>
-              <p className='workshops__item-name'>Rayana</p>
-              <p className='workshops__item-topic'>Saidi Choreografie</p>
-            </li>
-            <li className='workshops__item'>
-              <p className='workshops__item-time'>11:15-13:15</p>
-              <p className='workshops__item-name'>Yela</p>
-              <p className='workshops__item-topic'>Schleierfächer Choreografie</p>
-            </li>
-            <li className='workshops__item'>
-              <p className='workshops__item-time'>13:30-15:30</p>
+              <p className='workshops__item-time'>12:00-14:00</p>
               <p className='workshops__item-name'>Leandro Ferreyra</p>
-              <p className='workshops__item-topic'>Tango oriental Choreografie</p>
+              <p className='workshops__item-topic'>Saidi</p>
             </li>
             <li className='workshops__item'>
-              <p className='workshops__item-time'>15:45-17:45</p>
-              <p className='workshops__item-name'>Aliah </p>
-              <p className='workshops__item-topic'>Lyric</p>
+              <p className='workshops__item-time'>14:15-16:15</p>
+              <p className='workshops__item-name'>Sofiia Varshavskaia</p>
+              <p className='workshops__item-topic'>Pop</p>
             </li>
             <li className='workshops__item'>
-              <p className='workshops__item-time'>19:30</p>
-              <p className='workshops__item-name'>1001Nacht Opening Gala Show</p>
-              <p className='workshops__item-place'>Ort: Am Alten Gymnasium 1, Neuruppin</p>
+              <p className='workshops__item-time'>16:30-18:30</p>
+              <p className='workshops__item-name'>Aliah</p>
+              <p className='workshops__item-topic'>Mejansé</p>
+            </li>
+            <li className='workshops__item'>
+              <p className='workshops__item-time'>18:45-20:45</p>
+              <p className='workshops__item-name'>Nadine Etlah</p>
+              <p className='workshops__item-topic'>Balady</p>
             </li>
           </ul>
         </section>
         <section className='workshops__section'>
-          <h2 className='main__subheader workshops__white'>Samstag 07.10.2023</h2>
+          <h2 className='main__subheader workshops__white'>Samstag 12.10.2024</h2>
           <ul className='workshops__items'>
             <li className='workshops__item'>
-              <p className='workshops__item-time'>Ab 09:00-18:00</p>
+              <p className='workshops__item-time'>Ab 09:00-16:00</p>
               <p className='workshops__item-name'>Wettbewerbe</p>
             </li>
             <li className='workshops__item'>
-              <p className='workshops__item-time'>19:30</p>
+              <p className='workshops__item-time'>18:00</p>
               <p className='workshops__item-name'>1001Nacht Stars Gala Show</p>
-              <p className='workshops__item-place'>Ort: Am alten Gymnasium 1, Neuruppin</p>
             </li>
           </ul>
         </section>
         <section className='workshops__section'>
-          <h2 className='main__subheader workshops__white'>Sonntag 08.10.2023</h2>
+          <h2 className='main__subheader workshops__white'>Sonntag 13.10.2024</h2>
           <ul className='workshops__items'>
             <li className='workshops__item'>
-              <p className='workshops__item-time'>09:00-11:00</p>
-              <p className='workshops__item-name'>1. Rayana</p>
-              <p className='workshops__item-topic'>Tabla Solo Choreografie</p>
-              <p className='workshops__item-name'>2. Marina</p>
-              <p className='workshops__item-topic'>Baladi Choreografie</p>
+              <p className='workshops__item-time'>10:00-12:00</p>
+              <p className='workshops__item-name'>Leandro Ferreyra</p>
+              <p className='workshops__item-topic'>Trommel Solo</p>
             </li>
             <li className='workshops__item'>
-              <p className='workshops__item-time'>11:15-13:15</p>
-              <p className='workshops__item-name'>Shalymar</p>
-              <p className='workshops__item-topic'>Schleier Traum Choreografie</p>
+              <p className='workshops__item-time'>12:30-14:30</p>
+              <p className='workshops__item-name'>Svetare</p>
+              <p className='workshops__item-topic'>Fächerschleier</p>
             </li>
             <li className='workshops__item'>
               <p className='workshops__item-time'>13:30-15:30</p>
@@ -88,12 +80,9 @@ const Workshops: NextPage = () => {
               <p className='workshops__item-topic'>Mejance Choreografie</p>
             </li>
             <li className='workshops__item'>
-              <p className='workshops__item-time'>15:45-17:45</p>
-              <p className='workshops__item-name'>1. Amira</p>
-              <p className='workshops__item-topic'>Pop Choreografie</p>
-              <p className='workshops__item-name'>2. Aminahbanu </p>
-              <p className='workshops__item-topic'>Grundschritte + kl. TrommelSolo Choreografie</p>
-              <p className='workshops__item-place'>Ort: Am Alten Gymnasium 1, Neuruppin</p>
+              <p className='workshops__item-time'>16:00</p>
+              <p className='workshops__item-name'>1001Nacht Closing Gala</p>
+              <p className='workshops__item-topic'>Ort: Am Alten Gymnasium 1, Neuruppin</p>
             </li>
           </ul>
         </section>
